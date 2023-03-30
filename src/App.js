8b04d42696ceb21794c4028c9f43e7ff448ec008
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-29 17:39:43 -1200

    easy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AllTheBooks from './components/AllTheBooks';
 import MyFooter from './components/MyFooter';
 import MyNav from './components/MyNav';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <>
       <MyNav/>
+      <Welcome/>
+      <AllTheBooks/>
       <MyFooter/>
     </>
   );
